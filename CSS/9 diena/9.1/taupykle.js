d5bb355fe6cbf6366ebdb7936e920b4526881d17
iakinca <ilona.akinca@gmail.com>
2024-12-16 14:30:20 +0200

    Add JavaScript 9 diena 9.3 file

---
+++ CSS/9 diena/9.1/taupykle.js
@@ -0,0 +1,5 @@
+const monetosPo5 = +prompt("Kiek yra monetu po 5 ct");
+const monetosPo20 = +prompt("Kiek monetu po 20 ct");
+const monetosPo2 = +prompt("Kiek monetu po 2 eur");
+const result = monetosPo5 * 0.05 + monetosPo20 * 0.2 + monetosPo2 * 2;
+console.log("Taupykleje yra " + result + " Eur ");

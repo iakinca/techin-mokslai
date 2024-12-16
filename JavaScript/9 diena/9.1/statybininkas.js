916cf841d767a615a3fa004704541594d298930f
iakinca <ilona.akinca@gmail.com>
2024-12-16 22:01:53 +0200

    Add 9 diena JavaScript 9.5 file

--- JavaScript/9 diena/9.1/statybininkas.js
+++ JavaScript/9 diena/9.1/statybininkas.js
@@ -3,6 +3,9 @@ const plytuAusktis = 10;
 const sienosIlgis = +prompt("Sienos ilgis");
 const sienosAukstis = +prompt("Sienos aukstis");
 const plytosKaina = +prompt("Plytos kaina");
-const result = (sienosIlgis / plytuIlgis) * (sienosAukstis / plytuAusktis);
-console.log("Plytu kiekis: " + result);
-console.log("Plytos kainuos " + result * plytosKaina);
+const sienaCM = (sienosIlgis*sienosAukstis)*100;
+const plytaCm = (20*10)/100;
+const plytuKiekis = sienaCM/plytaCm;
+const plytuKaina = plytuKiekis*plytosKaina
+console.log("Plytu kiekis: " + result); 
+console.log("Plytos kainuos " + result * plytosKaina); 

75277cd7d821f8f8b0abc6dde637071f32e87e97
iakinca <ilona.akinca@gmail.com>
2024-12-16 23:13:44 +0200

    Add 9 diena JavaScript 9.7 file

---
+++ JavaScript/9 diena/9.1/trapecija.js
@@ -0,0 +1,5 @@
+const ilgesnis = +prompt("Ilgesniojo pagrindo ilgis");
+const trumpesnis = +prompt("Trumpesniojo pagrindo ilgis");
+const aukstis = +prompt("Aukstinės ilgis");
+const result = ((ilgesnis + trumpesnis) / 2) * aukstis;
+console.log('Trapecijos plotas: ${result}'); 

374ddf29e9f1419d60ee166074288ebf4ae4a9c3
iakinca <ilona.akinca@gmail.com>
2024-12-16 12:41:09 +0200

    Add JavaScript 9 diena 9.1 file

---
+++ CSS/9 diena/9.1/script.js
@@ -0,0 +1,10 @@
+// 1 uzduotis
+const pirmadienis = +prompt("Kiek pamoku yra pirmadieni?");
+const antradienis = +prompt("Kiek pamoku yra antradieni?");
+const treciadienis = +prompt("Kiek pamoku yra treciadieni?");
+const ketvirtadienis = +prompt("Kiek pamoku yra ketvirtadieni?");
+const penktadienis = +prompt("Kiek pamoku yra penktadieni?");
+const result =
+  pirmadienis + antradienis + treciadienis + ketvirtadienis + penktadienis;
+console.log("Pamoku skaicius:" + result);
+console.log("Tai sudaro minuciu:" + result * 45);

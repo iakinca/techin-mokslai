eeaa91b0a7de17752ab739e2a413438659472fc0
iakinca <ilona.akinca@gmail.com>
2024-12-17 10:38:31 +0200

    Add 10 diena JavaScript 10.1 file

--- JavaScript/9 diena/9.1/statybininkas.js
+++ JavaScript/9 diena/9.1/statybininkas.js
@@ -4,9 +4,9 @@ const sienosAukstis = +prompt("Sienos aukstis metrais");
 const plytosIlgis = 20;
 const plytosAukstis = 10;
 const plytosKaina = 0.5
-const sienaCM = (sienosIlgis * 100) * (sienosAukstis * 100)
+const sienaCm = (sienosIlgis * 100) * (sienosAukstis * 100)
 const plytaCm = plytosIlgis * plytosAukstis
-const plytuKiekis = sienaCM / plytaCm
+const plytuKiekis = sienaCm / plytaCm
 const plytuKaina = plytuKiekis * plytuKaina
 console.log('Plytu kiekis: ${plytosKaina}')
 console.log('Plytos kainuos: ${plytuKaina}')

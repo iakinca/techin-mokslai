a9e315808c4b62e45409f545d8aef9cfdab84df1
iakinca <ilona.akinca@gmail.com>
2024-12-18 13:17:23 +0200

    Add 11 diena JavaScript 11.1 file

---
+++ JavaScript/11 diena/11.1/svetaine/js/resto.js
@@ -0,0 +1,16 @@
+const button = document.querySelector(".btn.btn-primary.btn-shadow.btn-lg");
+console.log(button);
+const img = document.querySelector("img");
+console.log(img);
+const deliciuos = document.querySelector(".display-4.mb-5");
+console.log(deliciuos);
+const team = document.querySelector(
+  "#navbarSupportedContent > ul:nth-child(3) > div > li:nth-child(2) > a"
+);
+console.log(team);
+const search = document.querySelector("#side-search-open");
+console.log(search);
+const support = document.querySelector(
+  "body > div.boxed-page > div > div > div > div.col-lg-6.hero-left > ul > li:nth-child(3)"
+);
+console.log(support);

c001a735ced259918647b03191d30f2a1e89d7e7
iakinca <ilona.akinca@gmail.com>
2024-12-19 13:00:45 +0200

    Add 12 diena Javascript 12.1 file

--- JavaScript/12 diena/script.js
+++ JavaScript/12 diena/script.js
@@ -1,8 +1,10 @@
 const formOne = document.forms["form-one"];
+const pharagrah = document.querySelector("#sum");
 formOne.addEventListener("submit", function (e) {
   e.preventDefault();
   const firstNumber = Number(e.target["first-number"].value);
   const secondNumber = Number(e.target["second-number"].value);
   const sum = firstNumber + secondNumber;
-  console.log("Sum is: " + sum);
+  console.log(sum);
+  pharagrah.textContent = `${sum}`;
 });

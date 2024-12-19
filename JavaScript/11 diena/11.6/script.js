b7c6a5f86cea70ac62e311b2afef2d87d607ffe8
iakinca <ilona.akinca@gmail.com>
2024-12-19 16:24:38 +0200

    Add 12 diena Javascript 11.6 file

---
+++ JavaScript/11 diena/11.6/script.js
@@ -0,0 +1,30 @@
+const kaire = document.querySelector("#kaire");
+const desine = document.querySelector("#desine");
+const none = document.querySelector("#paslepti");
+const nukelti = document.querySelector("#nukelti");
+const atstatyti = document.querySelector("#atstatyti");
+
+const img = document.querySelector("img");
+
+// const parent = document.querySelector("body");
+// const p = document.querySelector("#lorem");
+
+kaire.addEventListener("click", function () {
+  img.style.float = "left";
+});
+
+desine.addEventListener("click", function () {
+  img.style.float = "right";
+});
+
+none.addEventListener("click", function () {
+  img.style.;
+});
+
+nukelti.addEventListener("click", function () {
+  img.style;
+});
+
+atstatyti.addEventListener("click", function () {
+  img.style;
+});

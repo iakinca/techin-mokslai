0c3d8a74b72d1388202cdde50b2b45c051122331
iakinca <ilona.akinca@gmail.com>
2024-12-20 15:04:38 +0200

    Add 11 diena JavaScript 11.7 file

---
+++ JavaScript/11 diena/11.7/script.js
@@ -0,0 +1,8 @@
+const button = document.querySelector("#komentaras");
+const form = document.querySelector("form");
+
+button.addEventListener("click", function () {
+  form.style.visibility = "visible";
+});
+
+alert("Dekojame uz Jusu nuomone.");

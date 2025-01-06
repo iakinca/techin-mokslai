8cbe63fa5e05c93531ab2f599ca85b2234868b2e
iakinca <ilona.akinca@gmail.com>
2025-01-06 15:54:32 +0200

    Add Atsiskaitymas file 1

---
+++ atsiskaitymas JavaScript/uzduotis 1/javascript.js
@@ -0,0 +1,11 @@
+const skaicius = +prompt("Iveskite skaiciu");
+let suma = 0;
+
+function sumUntil(n) {
+  let sum = 0;
+  for (let i = 2; i <= n; i += 2) {
+    sum += i;
+  }
+  return sum;
+}
+console.log(sumUntil(20));

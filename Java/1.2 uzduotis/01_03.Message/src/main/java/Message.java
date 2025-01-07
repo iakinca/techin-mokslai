78a98b97708dcbb49b756341374fa6ec41511df5
Akinca <artur.demianenko@stud.techin.lt>
2025-01-07 12:55:37 +0200

    Add Java file 1.3

--- Java/1.2 uzduotis/01_03.Message/src/main/java/Message.java
+++ Java/1.2 uzduotis/01_03.Message/src/main/java/Message.java
@@ -6,8 +6,9 @@ public class Message {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Write a message:");
-        // Write your program here
+        System.out.println("Write a message: ");
+        String message = scanner.nextLine();
+        System.out.println(message);
 
     }
 }

e1f33cb9fb80dd19a8ce409925649ed172aeaa08
Akinca <ilona.akinca@gmail.com>
2025-01-08 14:01:36 +0200

    Add Java 3.6 file

--- Java/3.1 uzduotis/03_06.Password/src/main/java/Password.java
+++ Java/3.1 uzduotis/03_06.Password/src/main/java/Password.java
@@ -6,6 +6,14 @@ public class Password {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Write your program here 
+        // Write your program here
+//        String password = "Caput Draconis";
+        System.out.println("Password?");
+String input = (scan.nextLine());
+        if (input.equals("Caput Draconis")) {
+            System.out.println("Welcome!");
+        } else {
+            System.out.println("Off with you!");
+        }
     }
 }

c581beea5453f229623df5b075da58dc25980eac
Akinca <ilona.akinca@gmail.com>
2025-01-09 11:17:47 +0200

    Add Java 3.8 file

--- Java/3.1 uzduotis/03_08.CheckingTheAge/src/main/java/CheckingTheAge.java
+++ Java/3.1 uzduotis/03_08.CheckingTheAge/src/main/java/CheckingTheAge.java
@@ -6,5 +6,12 @@ public class CheckingTheAge {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.print("How old are you?");
+        int age = scan.nextInt();
+        if (age >= 0 && age <= 120) {
+            System.out.print("OK");
+        } else {
+            System.out.print("Impossible!");
+        }
     }
 }

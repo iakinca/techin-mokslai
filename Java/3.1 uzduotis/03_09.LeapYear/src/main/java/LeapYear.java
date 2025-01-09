89ec50f8af9906f1caf4d4cc836bbf65a6f2dc39
Akinca <ilona.akinca@gmail.com>
2025-01-09 11:37:48 +0200

    Add Java 3.9 file

--- Java/3.1 uzduotis/03_09.LeapYear/src/main/java/LeapYear.java
+++ Java/3.1 uzduotis/03_09.LeapYear/src/main/java/LeapYear.java
@@ -6,5 +6,13 @@ public class LeapYear {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.print("Give a year:");
+        int year = scan.nextInt();
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.print("The year is a leap year.");
+        } else {
+            System.out.print("The year is not a leap year.");
+        }
+
     }
 }

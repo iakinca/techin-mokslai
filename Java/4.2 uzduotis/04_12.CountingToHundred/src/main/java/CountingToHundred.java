fda11aa61e709005fb7a9607f6cff29002e7b3d3
Akinca <ilona.akinca@gmail.com>
2025-01-10 13:25:14 +0200

    Add JAVA 4.12 file

--- Java/4.2 uzduotis/04_12.CountingToHundred/src/main/java/CountingToHundred.java
+++ Java/4.2 uzduotis/04_12.CountingToHundred/src/main/java/CountingToHundred.java
@@ -6,5 +6,10 @@ public class CountingToHundred {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int number = scanner.nextInt();
+
+        for (int i = number; i <= 100; i++) {
+            System.out.println(i);
+        }
     }
 }

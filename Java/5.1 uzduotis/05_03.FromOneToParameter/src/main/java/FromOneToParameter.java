10b3bbc2ff73bd728a8925dc3cf5aee93774e266
iakinca <ilona.akinca@gmail.com>
2025-01-12 18:14:26 +0200

    Add Java 5.3 file

--- Java/5.1 uzduotis/05_03.FromOneToParameter/src/main/java/FromOneToParameter.java
+++ Java/5.1 uzduotis/05_03.FromOneToParameter/src/main/java/FromOneToParameter.java
@@ -1,9 +1,17 @@
-
+import java.util.Scanner;
 
 public class FromOneToParameter {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        printUntilNumber(number);
     }
 
+    public static void printUntilNumber(int number) {
+        for (int i = 1; i <= number; i++) {
+            System.out.println(i);
+        }
+
+    }
 }

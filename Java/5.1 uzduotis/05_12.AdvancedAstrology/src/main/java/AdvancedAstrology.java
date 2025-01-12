e912407eabbc89105b93d0ac6e3b440cd61c8a4d
iakinca <ilona.akinca@gmail.com>
2025-01-12 20:28:02 +0200

    Add Java 5.12 file

--- Java/5.1 uzduotis/05_12.AdvancedAstrology/src/main/java/AdvancedAstrology.java
+++ Java/5.1 uzduotis/05_12.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -3,18 +3,47 @@ public class AdvancedAstrology {
 
     public static void printStars(int number) {
         // part 1 of the exercise
+        for (int i = 1; i <= number; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
     }
 
     public static void printSpaces(int number) {
         // part 1 of the exercise
+        for (int i = 1; i <= number; i++) {
+            System.out.print(" ");
+        }
     }
 
     public static void printTriangle(int size) {
         // part 2 of the exercise
+        for (int i = 1; i <= size; i++) {
+            printSpaces(size - i);
+            printStars(i);
+        }
     }
 
     public static void christmasTree(int height) {
         // part 3 of the exercise
+        int countOfStars = 1;
+        for (int i = 1; i <= height; i++) {
+            printSpaces(height - i);
+            printStars(countOfStars);
+            countOfStars = countOfStars + 2;
+        }
+
+        printBase(countOfStars - 2);
+    }
+
+    public static void printBase(int countOfStars) {
+        int height = 2;
+        int width = 3;
+
+        for (int i = 1; i <= height; i++) {
+            printSpaces((countOfStars - width) / 2);
+            printStars(width);
+        }
     }
 
     public static void main(String[] args) {

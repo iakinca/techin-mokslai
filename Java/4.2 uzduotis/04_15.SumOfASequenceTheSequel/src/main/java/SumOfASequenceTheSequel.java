21173b0cbb2cf53917fb0e14c00d26cfb00a36f2
iakinca <ilona.akinca@gmail.com>
2025-01-12 16:34:34 +0200

    Add JAVA 4.15 file

--- Java/4.2 uzduotis/04_15.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
+++ Java/4.2 uzduotis/04_15.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
@@ -6,5 +6,14 @@ public class SumOfASequenceTheSequel {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int firstNumber = scanner.nextInt();
+        int secondNumber = scanner.nextInt();
+        int sum = 0;
+
+        for (int i = firstNumber; i <= secondNumber; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+
     }
 }

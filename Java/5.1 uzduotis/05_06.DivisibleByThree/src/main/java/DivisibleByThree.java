2d10e783ce8119ec181e9a78ad82e4a56b82e885
iakinca <ilona.akinca@gmail.com>
2025-01-12 18:55:00 +0200

    Add Java 5.6 file

--- Java/5.1 uzduotis/05_06.DivisibleByThree/src/main/java/DivisibleByThree.java
+++ Java/5.1 uzduotis/05_06.DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -5,7 +5,18 @@ public class DivisibleByThree {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.print("Beginning: ");
+        int start = scanner.nextInt();
+        System.out.print("End: ");
+        int finish = scanner.nextInt();
+        divisibleByThreeInRange(start, finish);
     }
 
+    public static void divisibleByThreeInRange(int beginning, int end) {
+        for (int i = beginning; i <= end; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
 }

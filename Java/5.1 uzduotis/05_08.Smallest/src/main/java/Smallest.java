864e8376ced50702588e9d30ea2dcf98f3fa7ae6
iakinca <ilona.akinca@gmail.com>
2025-01-12 19:20:02 +0200

    Add Java 5.8 file

--- Java/5.1 uzduotis/05_08.Smallest/src/main/java/Smallest.java
+++ Java/5.1 uzduotis/05_08.Smallest/src/main/java/Smallest.java
@@ -2,10 +2,15 @@
 public class Smallest {
 
     public static int smallest(int number1, int number2) {
+        if (number1 > number2) {
+            return number2;
+        } else {
+            return number1;
+        }
+
         // Write some code here
         // don't print anything inside this method
         // there must be a return in the end of the method
-        return -1;
     }
 
     public static void main(String[] args) {

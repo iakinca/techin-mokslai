7b787f9b11a02fab2d08044c16373708bf2b1ef5
Akinca <ilona.akinca@gmail.com>
2025-01-13 14:19:47 +0200

    Add JAVA 6.5 file

--- Java/6.1 uzduotis/06_05.PrintInStars/src/main/java/Printer.java
+++ Java/6.1 uzduotis/06_05.PrintInStars/src/main/java/Printer.java
@@ -1,4 +1,3 @@
-
 public class Printer {
 
     public static void main(String[] args) {
@@ -9,6 +8,14 @@ public class Printer {
 
     public static void printArrayInStars(int[] array) {
         // Write some code in here
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i]; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 
 }

76af8fc80d73121b91b77d0332dd94a3dcfb61ac
Akinca <ilona.akinca@gmail.com>
2025-01-15 11:41:22 +0200

    Add Java 7.9 file

--- Java/7.1 Arraylist/AL_09.GreatestInList/src/main/java/GreatestInList.java
+++ Java/7.1 Arraylist/AL_09.GreatestInList/src/main/java/GreatestInList.java
@@ -16,9 +16,16 @@ public class GreatestInList {
 
             list.add(input);
         }
-        
+
         System.out.println("");
 
         // implement finding the greatest number in the list here
+        int greatest = list.get(0);
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) > greatest) {
+                greatest = list.get(i);
+            }
+        }
+        System.out.println("The greatest number: " + greatest);
     }
 }

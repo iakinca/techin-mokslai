8d30e3fe95aae8a0e933996ef51ee77daaddb185
Akinca <ilona.akinca@gmail.com>
2025-01-15 12:44:26 +0200

    Add Java 7.11 file

--- Java/7.1 Arraylist/AL_11.IndexOfSmallest/src/main/java/IndexOfSmallest.java
+++ Java/7.1 Arraylist/AL_11.IndexOfSmallest/src/main/java/IndexOfSmallest.java
@@ -9,11 +9,30 @@ public class IndexOfSmallest {
 
         // implement here a program that reads user input
         // until the user enters 9999
-        
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int input = scanner.nextInt();
+            if (input == 9999) {
+                break;
+            }
+            list.add(input);
+        }
         // after that, the program prints the smallest number
         // and its index -- the smallest number
         // might appear multiple times
+        int smallest = list.get(0);
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) < smallest) {
+                smallest = list.get(i);
+            }
+        }
+        System.out.println("Smallest number: " + smallest);
 
-        
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) == smallest)
+                System.out.println("Found at index: " + i);
+        }
+
+
     }
 }

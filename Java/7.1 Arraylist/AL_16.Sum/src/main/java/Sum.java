cac035ec1592f0b61b3e6ace78aa11ca229e1655
Akinca <ilona.akinca@gmail.com>
2025-01-17 07:50:52 +0200

    Add JAVA 7.16 file

--- Java/7.1 Arraylist/AL_16.Sum/src/main/java/Sum.java
+++ Java/7.1 Arraylist/AL_16.Sum/src/main/java/Sum.java
@@ -5,6 +5,25 @@ public class Sum {
 
     public static void main(String[] args) {
         // Try your method here
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(6);
+        numbers.add(-1);
+        System.out.println(sum(numbers));
+
+        numbers.add(5);
+        numbers.add(1);
+        System.out.println(sum(numbers));
+    }
+
+    public static int sum(ArrayList<Integer> numbers) {
+        int total = 0;
+        for (int i = 0; i < numbers.size(); i++) {
+            total += numbers.get(i);
+        }
+        return total;
     }
 
 }

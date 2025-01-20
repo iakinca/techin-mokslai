3fb6d1531a7f696a1dc8a5a2b587e8ec9128b53d
Akinca <ilona.akinca@gmail.com>
2025-01-20 13:45:47 +0200

    Add Java 8.11 file

--- Java/8.1 OP/08_11.NumberStatistics/src/main/java/MainProgram.java
+++ Java/8.1 OP/08_11.NumberStatistics/src/main/java/MainProgram.java
@@ -12,5 +12,27 @@ public class MainProgram {
         // correct order in the main program. First the object that tracks the total
         // sum, secondly the object that tracks the sum of even numbers, 
         // and lastly the one that tracks the sum of odd numbers!
+        Statistics allNumbers = new Statistics();
+        Statistics evenNumbers = new Statistics();
+        Statistics oddNumbers = new Statistics();
+
+        System.out.println("Enter numbers: ");
+
+        while (true) {
+            int number = Integer.parseInt(scanner.nextLine());
+            if (number == -1) {
+                break;
+            }
+            allNumbers.addNumber(number);
+
+            if (number % 2 == 0) {
+                evenNumbers.addNumber(number);
+            } else {
+                oddNumbers.addNumber(number);
+            }
+        }
+        System.out.println("Sum: " + allNumbers.sum());
+        System.out.println("Sum of even numbers: " + evenNumbers.sum());
+        System.out.println("Sum off odd numbers: " + oddNumbers.sum());
     }
 }

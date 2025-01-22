b20ff41c862459f780156c77213546c47fb0a9e3
Akinca <ilona.akinca@gmail.com>
2025-01-22 13:52:01 +0200

    Add JAVA 8.2.6 file

--- Java/8.2 uzduotis/08_2_06.OverloadedCounter/src/main/java/MainProgram.java
+++ Java/8.2 uzduotis/08_2_06.OverloadedCounter/src/main/java/MainProgram.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 
 public class MainProgram {
 
     public static void main(String[] args) {
         // Test your counter here
+
+        Counter c = new Counter(5);
+        c.increase(-2);
+        System.out.println(c.value());
     }
 }

bbe02036259289ca5adc71aeecd5449e43fce049
Akinca <ilona.akinca@gmail.com>
2025-01-22 10:58:46 +0200

    Add JAVA 8.2.3 file

--- Java/8.2 uzduotis/08_2_03.TelevisionPrograms/src/main/java/Main.java
+++ Java/8.2 uzduotis/08_2_03.TelevisionPrograms/src/main/java/Main.java
@@ -9,5 +9,26 @@ public class Main {
         ArrayList<TelevisionProgram> programs = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+
+            if (name.equals("")) {
+                break;
+            }
+            System.out.print("Duration: ");
+            int duration = Integer.parseInt(scanner.nextLine());
+
+            programs.add(new TelevisionProgram(name, duration));
+        }
+        System.out.print("Program's maximum duration? ");
+        int maxDuration = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < programs.size(); i++) {
+            if (programs.get(i).getDuration() <= maxDuration) {
+                System.out.println(programs.get(i));
+                
+            }
+        }
     }
 }

4bfcba1bc5c6c0b7c1a7987c6b38dd0a20f9edb1
iakinca <ilona.akinca@gmail.com>
2025-01-23 21:40:09 +0200

    Add JAVA 9.2.2 file

---
+++ Java/8.1 OP/Account 9.1 project/src/Account.java
@@ -0,0 +1,5 @@
+public class Account {
+    private int id;
+    private double balance;
+    
+}
--- Java/9.2/09_02.PersonAndSubclasses/src/main/java/Person.java
+++ Java/9.2/09_02.PersonAndSubclasses/src/main/java/Person.java
@@ -9,6 +9,7 @@ public class Person {
 
     @Override
     public String toString() {
-        return name + "\n " + address;
+        return name +  "\n  " + address;
     }
 }
+
